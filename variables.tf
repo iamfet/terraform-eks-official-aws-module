cdeaf5fe5345005b161a73dbe281b2a03d88128f
iamfet <fetuyee@gmail.com>
2025-06-03 16:00:06 +0000

    infra(terraform): add variables

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "vpc_cidr_block" {
+    description = "CIDR block for the VPC"
+    type        = string
+}
+
+variable "private_subnets_cidr" {
+    description = "CIDR blocks for the private subnets"
+    type        = list(string)
+}
+
+variable "public_subnets_cidr" {
+    description = "CIDR blocks for the public subnets"
+    type        = list(string)
+}
+
+variable "project_name" {
+    description = "Name of the project"
+    type        = string
+}
+
+variable "cluster_version" {
+    description = "Version of the EKS cluster"
+    type        = string
+}
+
