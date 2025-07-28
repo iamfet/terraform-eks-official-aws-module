b2500153f219cff8da7e1e78a03483b7820ca66a
iamfet <fetuyee@gmail.com>
2025-07-28 11:20:30 +0000

    refactor: combine eks and vpc

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,24 +1,59 @@
+data "aws_availability_zones" "azs" {
+  state = "available"
+} #queries AWS to provide the names of availability zones dynamically
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 6.0"
+
+  name            = "${var.project_name}-vpc"
+  cidr            = var.vpc_cidr_block
+  private_subnets = var.private_subnets_cidr
+  public_subnets  = var.public_subnets_cidr
+  azs             = data.aws_availability_zones.azs.zone_ids
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    "kubernetes.io/cluster/${var.project_name}-eks-cluster" = "shared" # Tags required for EKS to discover subnets
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.project_name}-eks-cluster" = "shared"
+    "kubernetes.io/role/elb"                                = 1 # Identifies this subnet for external load balancers
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.project_name}-eks-cluster" = "shared"
+    "kubernetes.io/role/internal-elb"                       = 1 # Identifies this subnet for internal services
+  }
+}
+
+
 
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.34.0"
+  version = "~> 21.0"
 
-  cluster_name    = "${var.project_name}-eks-cluster"
-  cluster_version = var.cluster_version
+  name               = "${var.project_name}-eks-cluster"
+  kubernetes_version = var.kubernetes_version
 
-  subnet_ids = module.myapp-vpc.private_subnets
-  vpc_id     = module.myapp-vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
 
   enable_cluster_creator_admin_permissions = true
-  cluster_endpoint_public_access           = true
+  endpoint_public_access                   = true
 
-
-  tags = {
-    environment = "development"
-    application = "${var.project_name}"
+  addons = {
+    coredns                = {}
+    eks-pod-identity-agent = { before_compute = true }
+    kube-proxy             = {}
+    vpc-cni                = { before_compute = true }
   }
-
 
   eks_managed_node_groups = {
     dev = {
@@ -31,8 +66,9 @@ module "eks" {
   }
 
 
-
-
-
+  tags = {
+    environment = "dev"
+    Terraform   = "true"
+  }
 }
 
